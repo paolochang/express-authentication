125cc65ad2e1ac0f401c29853a50fb4c8da33a9e
Paolo Chang <chanhwan.chang@gmail.com>
2021-11-18 22:28:54 -0500

    Add server middleware: morgan

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
 
 app.listen(PORT, () =>
   console.log(`✅ Server is listening on http://localhost:${PORT} 🚀`)
