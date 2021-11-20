ed6593101cedc4b04d5e344a24859a8007e2a17c
Paolo Chang <chanhwan.chang@gmail.com>
2021-11-19 19:46:47 -0500

    Add scratch APIs

---
+++ server/src/api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import * as authCtrl from "./auth.ctrl";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", authCtrl.register);
+authRouter.post("/login", authCtrl.login);
+authRouter.get("/check", authCtrl.check);
+authRouter.post("/logout", authCtrl.logout);
+
+const userRouter = express.Router();
+
+userRouter.get("/", authCtrl.read);
+userRouter.patch("/", authCtrl.update);
+userRouter.delete("/", authCtrl.remove);
+
+authRouter.use("/:id", userRouter);
