b23b7e0d1e22d4927b21b6fe93776e7c6b229f57
Paolo Chang <chanhwan.chang@gmail.com>
2021-11-19 22:14:52 -0500

    Add POST /api/auth/register method

--- server/src/api/auth.ctrl.js
+++ server/src/api/auth.ctrl.js
@@ -1,5 +1,38 @@
-export const register = (req, res) => {
-  res.send("Register");
+import User from "../models/User";
+
+export const register = async (req, res) => {
+  let isPasswordMatch = true;
+  let existUsername = false;
+  try {
+    const { username, password, confirmPassword, name } = req.body;
+
+    if (password !== confirmPassword) {
+      isPasswordMatch = false;
+      let error = new Error("Passwords are not match.");
+      error.status = 400;
+      throw error;
+    }
+
+    existUsername = await User.findByUsername(username);
+
+    if (existUsername) {
+      let error = new Error("Username is already been used.");
+      error.status = 409;
+      throw error;
+    }
+
+    const user = new User({
+      username,
+      password,
+      name,
+    });
+
+    await user.save();
+
+    return res.status(200).json({ user });
+  } catch (err) {
+    return res.status(err.status).json({ err: err.message });
+  }
 };
 
 export const login = (req, res) => {
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -3,7 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: String,
 });
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
 
 const User = mongoose.model("User", UserSchema);
 
