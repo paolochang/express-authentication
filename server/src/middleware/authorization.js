3322c4b4d1a009e54b21d1db6919053a4338cb73
Paolo Chang <chanhwan.chang@gmail.com>
2021-11-21 14:32:53 -0500

    Add authorization middleware and add it before GET check

---
+++ server/src/middleware/authorization.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import jwt from "jsonwebtoken";
+
+const authorization = (req, res, next) => {
+  const token = req.cookies.auth_token;
+  if (!token) {
+    console.log("TOKEN IS NOT FOUND");
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, process.env.JWT_KEY);
+    console.log("DATA:", data);
+    req.user = {
+      _id: data._id,
+      username: data.username,
+    };
+    return next();
+  } catch (err) {
+    return res.sendStatus(403);
+  }
+};
+
+export default authorization;
